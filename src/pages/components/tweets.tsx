32a0f19ed0270532841b8ffdc4cfd0b911bbca78
PopsWC <andrewpappas77@gmail.com>
2023-01-30 07:48:21 -0500

    fixed errors

--- src/pages/components/tweets.tsx
+++ src/pages/components/tweets.tsx
@@ -14,7 +14,7 @@ const Tweets = (tweetData: TweetData) => {
     const likeMutation = tweetRouter.like.useMutation().mutateAsync;
     const unlikeMutation = tweetRouter.unlike.useMutation().mutateAsync;
     const hasLiked = tweetRouter.getTweetById.useQuery({ tweetId: tweetData.tweetId }).data!.likes.length > 0
-    const likes = tweetRouter.getTweetById.useQuery({ tweetId: tweetData.tweetId }).data?.likes?.length
+    const numLikes = tweetRouter.getTweetById.useQuery({ tweetId: tweetData.tweetId }).data?.likes?.length
 
     return (
         <a href="#" className="block max-w-sm p-6 min-w-full bg-white border-b border-gray-200 rounded-md shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
@@ -32,14 +32,14 @@ const Tweets = (tweetData: TweetData) => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" className={hasLiked ? "w-6 h-6 stroke-sky-700" : "w-6 h-6 stroke-white"}>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                     </svg>
-                    <span className="font-semibold text-white pl-1">{likes}</span>
+                    <span className="font-semibold text-white pl-1">{numLikes}</span>
                 </button>
 
                 <button className="inline-flex items-center text-center text-white font-semibold">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" className={hasLiked ? "w-6 h-6 stroke-sky-700" : "w-6 h-6 stroke-white"}>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
                     </svg>
-                    <span className="font-semibold text-white pl-1">{likes}</span>
+                    <span className="font-semibold text-white pl-1">{numLikes}</span>
                 </button>
 
                 <button className="inline-flex items-center text-center text-white font-semibold">
