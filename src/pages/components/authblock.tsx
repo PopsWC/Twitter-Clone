9d71272d9b091d402ad774c983cc20eb005f9a28
PopsWC <andrewpappas77@gmail.com>
2023-02-03 02:32:49 -0500

    styled authblock

--- src/pages/components/authblock.tsx
+++ src/pages/components/authblock.tsx
@@ -9,7 +9,7 @@ export function AuthBlock() {
         const userId = session.user.id
         return (
 
-            <div className="flex flex-col w-full max-w-sm h-full border-2 border-black rounded-lg shadow-md bg-white">
+            <div className="flex flex-col w-full h-full border-2 border-black rounded-lg shadow-md bg-white">
                 <div className="flex flex-row min-w-full items-center ml-3 mt-3">
                     <div className="flex flex-col w-auto p-3">
                         <Link href={"/profile/" + userId}>
@@ -23,11 +23,11 @@ export function AuthBlock() {
                     </div>
                 </div>
                 <div className="flex flex-row w-full h-auto items-center justify-center">
-                    <div className="flex flex-col">
+                    <div className="flex flex-col justify-center">
                         <span className="text-black text-sm">Buddy/Guy Best CSGO Player</span>
                     </div>
                 </div>
-                <div className="flex flex-row max-w-full h-auto items-center justify-center gap-3 mt-10">
+                <div className="flex flex-row w-full h-auto items-center justify-center gap-3 mt-10">
                     <div className="flex flex-col">
                         <button type="button" className="text-white bg-sky-400 hover:bg-sky-700/90 focus:ring-4 focus:outline-none focus:ring-sky-700/50 font-small rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:focus:ring-sky-700/55">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4 mr-1">
@@ -61,14 +61,23 @@ export function AuthBlock() {
         )
     }
 
-    return(
-    <div className="flex flex-col w-full max-w-sm h-full border border-gray-700 rounded-lg shadow-md bg-white">
-        <div className="flex flex-row min-w-full items-center ml-3 mt-3">
-            <div className="flex flex-col w-auto p-3 justify-center items-center">
-                <Link href="/api/auth/signin/discord" className="text-blue-700">Sign in</Link>
+    return (
+        <div className="flex flex-col w-full max-w-sm h-full border-2 border-black rounded-lg shadow-md bg-white">
+            <div className="flex flex-row min-w-full justify-center my-auto">
+                <div className="flex flex-col w-auto h-full p-3">
+                    <Link href="/api/auth/signin/discord">
+                        <button className="inline-flex items-center bg-sky-400 text-white text-lg font-semibold rounded-lg py-3 px-10">
+                            Sign In With Discord
+                            <svg stroke="white" xmlns="http://www.w3.org/2000/svg" fill="white" className="bi bi-discord ml-2 w-6 h-6" viewBox="0 0 16 16">
+                                <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z" fill="white"></path>
+                            </svg>
+                        </button>
+
+                    </Link>
+                </div>
             </div>
         </div>
-    </div>
-     )
+
+    )
 }
 export default AuthBlock
--- src/pages/components/tweets.tsx
+++ src/pages/components/tweets.tsx
@@ -45,7 +45,8 @@ const Tweets = (tweetData: TweetData) => {
     const numComments = api.tweetRouter.getComments.useQuery({ tweetId: tweetData.tweetId }).data?.length
 
 
-    const [showModal, setShowModal] = useState(false);
+    const [showReplyModal, setReplyModal] = useState(false);
+
 
     const tweetQuery = api.tweetRouter.list.useInfiniteQuery(
         {
@@ -67,14 +68,14 @@ const Tweets = (tweetData: TweetData) => {
 
     const { register, handleSubmit, reset } = useForm<Inputs>();
     const onSubmit: SubmitHandler<Inputs> = async data => {
-                await addPost.mutateAsync({tweet : data.tweet, parentId: tweetData.tweetId}).catch((err) => console.log(err))
-                reset({ tweet: "" })
+        await addPost.mutateAsync({ tweet: data.tweet, parentId: tweetData.tweetId }).catch((err) => console.log(err))
+        reset({ tweet: "" })
     };
 
 
 
     return (
-        <div className="block max-w-sm p-6 my-1 border-2 border-black min-w-full rounded-md bg-white hover:bg-slate-100">
+        <a className="block max-w-sm p-6 my-1 border-2 border-black min-w-full rounded-md bg-white hover:bg-slate-100">
             <Link href={'/profile/'} className="mb-2 text-lg font-bold tracking-tight text-black hover:underline underline-offset-4">@{tweetData.username}</Link>
             <p className="font-normal px-10 py-3 text-black text-md">{tweetData.tweet}</p>
             <div className="flex flex-row justify-end gap-3 pt-2">
@@ -106,27 +107,23 @@ const Tweets = (tweetData: TweetData) => {
                     <span className="font-semibold text-black pl-1">{numshares ? numshares : 0}</span>
                 </button>
 
-                <button className="inline-flex items-center text-center text-white font-semibold" onClick={() => setShowModal(true)}>
+                <button className="inline-flex items-center text-center text-white font-semibold" onClick={() => setReplyModal(true)}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="black" className={commentData ? "w-6 h-6 stroke-sky-400" : "w-6 h-6 stroke-black"}>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
                     </svg>
                     <p className="font-semibold text-black pl-1">{numComments ? numComments : 0}</p>
                 </button>
-                {showModal ? (
+                {showReplyModal ? (
                     <div className="fixed flex inset-0 h-full w-full bg-slate-100 items-center justify-center">
-                        <div className="inline-flex flex-col w-1/2 h-2/3 border-2 p-2 border-black rounded-lg shadow-md bg-white ">
-                            <div className="flex flex-row min-w-full p-4 items-center">
-                                <span className="text-black text-lg font-semibold">Reply To Tweet</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="black" className="w-6 h-6 ml-2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25" />
-                                </svg>
-                                <button className="flex flex-row ml-auto" onClick={() => setShowModal(false)}>
+                        <div className="inline-flex flex-col w-1/2 max-h-2/3 border-2 p-2 border-black rounded-lg shadow-md bg-white ">
+                            <div className="flex flex-row min-w-full p-4">
+                                <button className="flex flex-row ml-auto" onClick={() => setReplyModal(false)}>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="black" className="w-7 h-7">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                                     </svg>
                                 </button>
                             </div>
-                            <div className="flex flex-row pl-10 pt-5">
+                            <div className="flex flex-row pl-10">
                                 <span className="mb-2 text-lg font-semibold tracking-tight text-black">@{tweetData.username}</span>
                             </div>
                             <div className="flex flex-row pl-14">
@@ -144,20 +141,27 @@ const Tweets = (tweetData: TweetData) => {
                                 <div className="flex flex-row w-full bg-white">
                                     <div className="flex flex-row w-full">
                                         <ul className="flex flex-col w-full">
-                                            {tweetQuery.data?.pages.map((page, index) => (
-                                                <li key={page.items[0]?.id || index}>
-                                                    {page.items.map((item) => (
-                                                        // eslint-disable-next-line react/jsx-key
-                                                        <Tweets tweet={item.tweet} username={item.userName} userId={item.userId} tweetId={item.id} createdAt={item.createdAt.toDateString()} />
-                                                    ))}
-                                                </li>
-                                            ))}
+                                            {tweetQuery.data?.pages.length == undefined ? (
+                                                tweetQuery.data?.pages.map((page, index) => (
+                                                    <li key={page.items[0]?.id || index}>
+                                                        {page.items.map((item) => (
+                                                            // eslint-disable-next-line react/jsx-key
+                                                            <Tweets tweet={item.tweet} username={item.userName} userId={item.userId} tweetId={item.id} createdAt={item.createdAt.toDateString()} />
+                                                        ))}
+                                                    </li>
+                                                ))
+                                            ) : (
+                                                <div className="flex flex-row w-full h-full items-center justify-center border-2 border-black rounded-md p-10">
+                                                <span className="text-black text-md font-semibold">No replies yet you should make one</span>
+                                                </div>
+                                            )}
                                         </ul>
+
                                     </div>
                                 </div>
                             </div>
                             <div className="flex flex-row w-full h-1/6 items-end p-3">
-                                  {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+                                {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
                                 <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col p-3 w-full h-full border border-black rounded-lg shadow-md bg-white flex-wrap">
                                     <div className="flex flex-row w-4/5 h-full">
                                         <textarea
@@ -179,7 +183,7 @@ const Tweets = (tweetData: TweetData) => {
                     </div>
                 ) : null}
             </div>
-        </div>
+        </a>
     )
 }
 
